769c79b4986bd7672e3b658b720b96b106ab3df5
MadCabbagee <42429636+MadCabbagee@users.noreply.github.com>
2024-02-21 15:32:55 -0700

    run console UI

--- src/main/java/me/madcabbage/shopping_system/Main.java
+++ src/main/java/me/madcabbage/shopping_system/Main.java
@@ -1,8 +1,16 @@
 package me.madcabbage.shopping_system;
 
+import me.madcabbage.shopping_system.console_ui.ConsoleUI;
+
 public class Main {
 
     public static void main(String[] args) {
-        
+        runConsoleUI();
     }
+
+    private static void runConsoleUI() {
+        ConsoleUI.run();
+    }
+
+
 }
