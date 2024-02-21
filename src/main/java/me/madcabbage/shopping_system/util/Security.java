aed93375f990f2edd0343a3a8504a54c9ce1d47a
MadCabbagee <42429636+MadCabbagee@users.noreply.github.com>
2024-02-21 15:33:15 -0700

    stub method for hasingPasswords

---
+++ src/main/java/me/madcabbage/shopping_system/util/Security.java
@@ -0,0 +1,12 @@
+package me.madcabbage.shopping_system.util;
+
+public class Security {
+
+    private Security() {
+
+    }
+    public static String hashPassword(String rawPassword) {
+        // todo implement password hasing
+        return null;
+    }
+}
