d8acf9a85141542f6e77ac4802bad6026c8252cf
MadCabbagee <42429636+MadCabbagee@users.noreply.github.com>
2024-02-21 15:35:52 -0700

    add member fields, constructor, getters

---
+++ src/main/java/me/madcabbage/shopping_system/user/User.java
@@ -0,0 +1,40 @@
+package me.madcabbage.shopping_system.user;
+
+public abstract class User {
+
+    protected final String email;
+    protected final String hashedPassword;
+    protected final String firstName;
+    protected final String lastName;
+    protected final String phoneNumber;
+    protected final AccountType accountType;
+
+    protected User(String email, String hashedPassword, String firstName, String lastName, String phoneNumber, AccountType accountType) {
+        this.email = email;
+        this.hashedPassword = hashedPassword;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phoneNumber = phoneNumber;
+        this.accountType = accountType;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public AccountType getAccountType() {
+        return accountType;
+    }
+}
