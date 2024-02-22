3936053cefa34c16d29b7c44f57ef34514ea4435
MadCabbagee <42429636+MadCabbagee@users.noreply.github.com>
2024-02-22 05:27:03 -0700

    add some menu options, stub show() method, hide public constructor

---
+++ src/main/java/me/madcabbage/shopping_system/console_ui/menus/BuyerMenus.java
@@ -0,0 +1,16 @@
+package me.madcabbage.shopping_system.console_ui.menus;
+
+public class BuyerMenus {
+
+    private static final String[] buyerMenuOptions = { "Products", "Account Options","Logout"};
+    private static final String[] buyerProductsMenu = { "Browse", "Search", "Back"};
+    private static final String[] buyerProductActions = { "" };
+
+    private BuyerMenus() {
+        // Hide implicit constructor
+    }
+
+    public static void show() {
+
+    }
+}
