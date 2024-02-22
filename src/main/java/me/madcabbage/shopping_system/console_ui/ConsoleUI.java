cd98af0a76d39eaf626b39f2dffc3be1c17f50b9
MadCabbagee <42429636+MadCabbagee@users.noreply.github.com>
2024-02-22 05:30:11 -0700

    handle control flow for login of multiple account types. hide public constructor

--- src/main/java/me/madcabbage/shopping_system/console_ui/ConsoleUI.java
+++ src/main/java/me/madcabbage/shopping_system/console_ui/ConsoleUI.java
@@ -1,22 +1,21 @@
 package me.madcabbage.shopping_system.console_ui;
 
-import me.madcabbage.shopping_system.user.AccountType;
-import me.madcabbage.shopping_system.user.BuyerUser;
-import me.madcabbage.shopping_system.user.SellerUser;
+import me.madcabbage.shopping_system.console_ui.menus.AdminMenus;
+import me.madcabbage.shopping_system.console_ui.menus.BuyerMenus;
+import me.madcabbage.shopping_system.console_ui.menus.SellerMenus;
 import me.madcabbage.shopping_system.user.User;
 import me.madcabbage.shopping_system.util.Console;
-import me.madcabbage.shopping_system.util.Security;
 
 public class ConsoleUI {
+    private static final String[] mainMenuOptions = { "Login", "Register", "Reset Password" };
 
     private static boolean run;
-    private static final String[] mainMenuOptions = { "Login", "Register", "Reset Password" };
-    private static final String[] sellerMenuOptions = { "Add Product", "Remove Product", "Edit Product", "View Products" };
 
-    private static final String[] buyerMenuOptions = { "Browse" };
-    private static final String[] buyerProductOptions = { "" };
+    private ConsoleUI() {
+        // hide implicit constructor
+    }
 
-    public static void run() {
+    public static void show() {
         run = true;
         while (true) {
             String mainMenuInput = Console.promptWithChoicesSpaced("Main Menu:", mainMenuOptions);
@@ -24,80 +23,40 @@ public class ConsoleUI {
         }
     }
 
-    private static void handleMainMenuInput(String mainMenuInput) {
+    // Main Menu
+    public static void handleMainMenuInput(String mainMenuInput) {
         if (mainMenuInput.equals(mainMenuOptions[0])) {
-            loginUser();
+            // todo store current user somewhere globally
+            User loggedIn = LoginUI.loginUser();
+            if (loggedIn == null) {
+                return;
+            }
+            switch (loggedIn.getAccountType()) {
+                case Buyer -> BuyerMenus.show();
+                case Seller -> SellerMenus.show();
+                case Admin -> AdminMenus.show();
+                default -> throw new IllegalArgumentException("Invalid account type");
+            }
         } else if (mainMenuInput.equals(mainMenuOptions[1])) {
-            registerUser();
+            RegistrationUI.registerUser();
         } else if (mainMenuInput.equals(mainMenuOptions[2])) {
-            resetUserPassword();
+            LoginUI.resetUserPassword();
         }
     }
 
-    private static void loginUser() {
-        Console.print("Login:");
-        String username = Console.promptSpaced("Enter email:");
-        String password = Console.promptSpaced("Enter password:");
-        // todo check username and password from db with input
+    // Buyer Menus
+    public static void handleBuyerMenu() {
+
     }
 
-    private static void registerUser() {
-        Console.print("Register:");
-        AccountType accountType = getAccountType();
-        String email = getValidEmail();
-        String firstName = Console.promptSpaced("Enter username:");
-        String lastName = Console.promptSpaced("Enter last name:");
-        String phoneNumber = Console.promptSpaced("Enter phone number:");
-        String password = getValidPassword();
-        String hashedPassword = Security.hashPassword(password);
-        User registered = null;
-        switch (accountType) {
-            case Buyer -> registered = new BuyerUser(email, hashedPassword, firstName, lastName, phoneNumber);
-            case Seller -> registered = new SellerUser(email, hashedPassword, firstName, lastName, phoneNumber);
-            default -> throw new IllegalArgumentException(); // todo create custom exception for invalid account type
-        }
-        // todo add this user to userDB
+    public static void handleBuyerProductsMenu() {
+
     }
 
-    private static AccountType getAccountType() {
-        String[] accTypes = new String[] { "Buyer", "Seller" };
-        String accType = Console.promptWithChoicesSpaced("What account type are you registering for:", accTypes);
+    // Seller Menus
+    public static void handleSellerMenuOptions() {
 
-        if (accType.equals(accTypes[0])) {
-            return AccountType.Buyer;
-        } else if (accType.equals(accTypes[1])) {
-            return AccountType.Seller;
-        }
-        // todo create custom exception for invalid account type
-        throw new IllegalArgumentException();
     }
 
-    private static String getValidEmail() {
-        while (true) {
-            String email1 = Console.promptSpaced("Enter valid email:");
-            String email2 = Console.promptSpaced("Confirm valid email:");
-            if (email1.equals(email2)) {
-                return email1;
-            } else {
-                Console.printSpaced("Emails do not match");
-            }
-        }
-    }
-
-    private static String getValidPassword() {
-        while (true) {
-            String p1 = Console.promptSpaced("Enter desired password:");
-            String p2 = Console.promptSpaced("Confirm password:");
-            if (p1.equals(p2)) {
-                return p1;
-            } else {
-                Console.printSpaced("Passwords do not match");
-            }
-        }
-    }
-
-    private static void resetUserPassword() {
-        // todo reset user password using email
-    }
 
 }
