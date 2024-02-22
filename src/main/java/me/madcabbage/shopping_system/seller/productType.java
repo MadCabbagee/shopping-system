89963b060df37d7ea1ae2cc493b16e3a826a381a
Ahmed Mujtaba <ahmedmujtaba.ult@gmail.com>
2024-02-22 19:46:13 +0500

    Added product types for enum Added function to get the product type name

--- src/main/java/me/madcabbage/shopping_system/seller/productType.java
+++ src/main/java/me/madcabbage/shopping_system/seller/productType.java
@@ -1,5 +1,25 @@
 package me.madcabbage.shopping_system.seller;
 
 public enum productType {
+    BEAUTY_PRODUCTS("Beauty Products"),
+    BOOKS_AND_STATIONARY("Books and Stationary"),
+    CABBAGES("Cabbages"),
+    CAMPING_GOODS("Camping Goods"),
+    CLOTHING("Clothing"),
+    ELECTRONICS("Electronics"),
+    FURNITURE("Furniture"),
+    GROCERIES("Groceries"),
+    GUNS("Guns"),
+    HOME_KITCHEN_APPLIANCES("Home And Kitchen Appliances"),
+    MEDICINE("Medicine"),
+    SPORTS_GOOD("Sports Goods"),
+    TOYS_AND_GAMES("Toys And Games");
 
+    private final String productName;
+    private productType(String productName) {
+        this.productName = productName;
+    }
+    public String getProductName() {
+        return productName;
+    }
 }
