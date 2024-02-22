f08b7852cc39ded325d3214baa892fa422b2aa9e
MadCabbagee <42429636+MadCabbagee@users.noreply.github.com>
2024-02-22 05:30:36 -0700

    stub getUser method

---
+++ src/main/java/me/madcabbage/shopping_system/util/FileDatabase.java
@@ -0,0 +1,9 @@
+package me.madcabbage.shopping_system.util;
+
+import me.madcabbage.shopping_system.user.User;
+
+public class FileDatabase {
+    public static User getUser(String email) {
+        throw new RuntimeException(FileDatabase.class.getName() + "::getHashedPassword() is not implemented.");
+    }
+}
