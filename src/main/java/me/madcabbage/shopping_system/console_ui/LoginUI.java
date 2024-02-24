2be22cfe262f4c5cc2121e24a4bdc9d313355e60
MadCabbagee <42429636+MadCabbagee@users.noreply.github.com>
2024-02-24 01:57:51 -0700

    extract user authentication to Auth.authenticate

--- src/main/java/me/madcabbage/shopping_system/console_ui/LoginUI.java
+++ src/main/java/me/madcabbage/shopping_system/console_ui/LoginUI.java
@@ -1,5 +1,6 @@
 package me.madcabbage.shopping_system.console_ui;
 
+import me.madcabbage.shopping_system.auth.Auth;
 import me.madcabbage.shopping_system.user.User;
 import me.madcabbage.shopping_system.util.Console;
 import me.madcabbage.shopping_system.util.FileDatabase;
@@ -15,16 +16,8 @@ public class LoginUI {
         Console.print("Login:");
         String email = Console.prompt("Enter email:");
         String password = Console.promptSpaced("Enter password:");
-        // todo check username and password from db with input
-        // if valid login then return true
-        User user = FileDatabase.getUser(email);
-        try {
-            Security.hashPassword(password).equals(user.getHashedPassword());
-        } catch (NullPointerException e) {
-            Console.printSpaced("Invalid login information.");
-        } finally {
-            return user;
-        }
+
+        return Auth.authenticate(email, password);
     }
 
     public static void resetUserPassword() {
