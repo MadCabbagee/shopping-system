a9f28d563063f3b637d08bae1a13605f6f49c480
MadCabbagee <42429636+MadCabbagee@users.noreply.github.com>
2024-02-25 07:41:51 -0700

    fix formatting

--- src/main/java/me/madcabbage/shopping_system/products/Seller.java
+++ src/main/java/me/madcabbage/shopping_system/products/Seller.java
@@ -1,5 +1,7 @@
 package me.madcabbage.shopping_system.products;
+
 import me.madcabbage.shopping_system.products.Product;
+
 import java.util.ArrayList;
 
 
@@ -10,20 +12,20 @@ public class Seller {
     private ArrayList<Product> products; //Array list to add the sellers products
 
     //create new seller
-   public Seller(String name, String description, ArrayList<Product> productArrayList){
+    public Seller(String name, String description, ArrayList<Product> productArrayList) {
 
-       this.name = name;
-       this.description = description;
-       this.products = productArrayList;
+        this.name = name;
+        this.description = description;
+        this.products = productArrayList;
     }
 
     //seller but no product given
-    public Seller(String name, String description){
-       this(name,description,new ArrayList<Product>());
+    public Seller(String name, String description) {
+        this(name, description, new ArrayList<>());
     }
 
     //seller initialization but one product is given
-    public Seller(String name, String description, Product product){
+    public Seller(String name, String description, Product product) {
         //Not sure yet if this works maybe create a function for it
         this.name = name;
         this.description = description;
@@ -35,9 +37,11 @@ public class Seller {
     public String getName() {
         return name;
     }
+
     public String getDescription() {
         return description;
     }
+
     public ArrayList<Product> getProductArrayList() {
         return products;
     }
@@ -47,34 +51,37 @@ public class Seller {
     public void setName(String name) {
         this.name = name;
     }
+
     public void setDescription(String description) {
         this.description = description;
     }
+
     public void setProductArrayList(ArrayList<Product> productArrayList) {
         this.products = productArrayList;
     }
 
     //adder Methods
-    public void addProduct(Product product){
-       products.add(product);
+    public void addProduct(Product product) {
+        products.add(product);
     }
-    public void addProduct(ArrayList<Product> productArrayList){
+
+    public void addProduct(ArrayList<Product> productArrayList) {
 
         for (int i = 0; i < productArrayList.size(); i++) {
             Product product = productArrayList.get(i);
             addProduct(product);
         }
     }
-    public void removeProduct(Product productToBeDeleted)
-    {
-       products.remove(productToBeDeleted);
-    }
-    public void removeProduct(ArrayList<Product> productArrayList){
 
-       for (Product product : productArrayList)
-       {
-           removeProduct(product);
-       }
+    public void removeProduct(Product productToBeDeleted) {
+        products.remove(productToBeDeleted);
+    }
+
+    public void removeProduct(ArrayList<Product> productArrayList) {
+
+        for (Product product : productArrayList) {
+            removeProduct(product);
+        }
     }
 }
 
