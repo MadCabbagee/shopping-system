b3c6e0932ea581e6e45406a97b92684f7e54a3c5
MadCabbagee <42429636+MadCabbagee@users.noreply.github.com>
2024-02-25 02:13:57 -0700

    add comments, rename methods

--- src/main/java/me/madcabbage/shopping_system/util/FileDatabase.java
+++ src/main/java/me/madcabbage/shopping_system/util/FileDatabase.java
@@ -11,8 +11,10 @@ public class FileDatabase {
     private static final String applicationFolder = ".shoppingSystem";
     private static final String databaseFolder = "database";
     private static final String usersFolder = "users";
+    private static final String productsFolder = "products";
     private static final Path dbPath = Path.of(System.getProperty("user.home")).resolve(applicationFolder).resolve(databaseFolder);
     private static final Path userDbPath = dbPath.resolve(usersFolder);
+    private static final Path productsDbPath = dbPath.resolve(productsFolder);
 
     private FileDatabase() {
         // hide implicit constructor
@@ -28,10 +30,9 @@ public class FileDatabase {
 
     public static User getUser(String email) {
         try (FileInputStream fis = new FileInputStream(String.valueOf(userDbPath.resolve(email + ".ser")));
-        ObjectInputStream ois = new ObjectInputStream(fis)) {
+             ObjectInputStream ois = new ObjectInputStream(fis)) {
             return (User) ois.readObject();
-        }
-        catch (IOException | ClassNotFoundException e) {
+        } catch (IOException | ClassNotFoundException ignored) {
             // intentionally left empty
             // if exception is thrown method will return null
         }
@@ -40,12 +41,83 @@ public class FileDatabase {
 
     public static boolean saveUser(User user) {
         try (FileOutputStream fos = new FileOutputStream(String.valueOf(String.valueOf(userDbPath.resolve(user.getEmail() + ".ser"))));
-        ObjectOutputStream oos = new ObjectOutputStream(fos)) {
+             ObjectOutputStream oos = new ObjectOutputStream(fos)) {
             oos.writeObject(user);
             return true;
         } catch (IOException e) {
             return false;
         }
     }
+
+    // todo put each sellers products in separate folder
+    public static Product getProduct(int id) {
+        File productsDb = new File(productsDbPath.toString());
+        File[] sellerDirs = productsDb.listFiles();
+        if (sellerDirs == null) {
+            return null;
+        }
+        for (File sellerDir : sellerDirs) {
+            File productPath = new File(sellerDir, id + ".ser");
+            if (productPath.exists()) {
+                try (FileInputStream fis = new FileInputStream(productFile);
+                     ObjectInputStream ois = new ObjectInputStream(fis)) {
+                    return (Product) ois.readObject();
+                } catch (IOException | ClassNotFoundException ignored) {
+                    // intentionally left empty
+                }
+            }
+        }
+
+        return null;
+    }
+
+    public static boolean saveProduct(Product product) {
+        // todo: save product in sellers folder
+        try (FileOutputStream fos = new FileOutputStream(productsDbPath.resolve(product.getSeller().getName()).resolve(product.getId() + ".ser").toString());
+             ObjectOutputStream oos = new ObjectOutputStream(fos)) {
+            oos.writeObject(product);
+            return true;
+        } catch (IOException e) {
+            return false;
+        }
+    }
+
+    // todo: cache results of this method
+    public static Product[] getAllProducts() {
+        // todo Loop thru each seller folder
+        File[] productFiles = new File(String.valueOf(productsDbPath)).listFiles();
+        int offset = 0;
+
+        Product[] products = new Product[productFiles.length];
+        for (int i = 0; i < productFiles.length; i++) {
+            try {
+                int id = Integer.parseInt(productFiles[i].getName());
+                products[i - offset] = getProduct(id);
+            } catch (NumberFormatException ignored) {
+                offset++;
+            }
+        }
+
+        return products;
+    }
+
+    public static Product[] getProductsBySeller(Seller seller) {
+        // todo get products from each sellers folder
+        Product[] allProducts = getAllProducts();
+
+        int offset = 0;
+        Product[] fromSeller = new Product[15];
+        for (int i = 0; i < allProducts.length; i++) {
+            Product p = allProducts[i];
+            if (p.getSeller.equals(seller)) {
+                fromSeller[i - offset] = p;
+            } else {
+                offset++;
+            }
+        }
+        return fromSeller;
+    }
+
+
 
 }
