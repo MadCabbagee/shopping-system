b068ccd8ce0902a9dd6086b59e041ccc65056ff4
MadCabbagee <42429636+MadCabbagee@users.noreply.github.com>
2024-02-25 02:18:57 -0700

    Shopping cart (#7)
    * Addition of SRS (#3)
    
    * Renamed Scope
    
    * Addition of SRS Document
    
    * Addition of Product Class (#2)
    
    * Addition of the product class
    
    * added public constructor
    change instances data types to private
    Changed seller data type to Seller
    
    * Addition of the Seller Class (#4)
    
    * Addition of the Seller Class
    
    * Creation of different types of constructors
    Addition of adder and remover methods for Seller
    
    * Minor changes to the product and seller class, creation of enum dataType (#5)
    
    * Imported class ArrayList
    Imported Class Product
    Renamed addProductArrayList to addProduct
    Renamed removedProductArrayList to remove Product
    
    * Removed unused import MidiUnavailableException;
    
    * Imported Seller class in Product.java
    
    * Created Enum productType.java
    
    * Added product types for enum
    Added function to get the product type name
    
    * Renamed enum productType.java to ProductType.java
    Added 'UNCATEGORIZED' product type
    
    * Removed ProductType import from seller
    Refactored ProductType.java to Product package
    
    * removed final form ProductType productName
    
    * Changed productType data type in Product.java from string to ProductType.
    Changed the setter, getter, and the constructor accordingly.
    
    * Added package functionalities
    
    * Added Class ShoppingCart.java
    
    * Created Class attributes, getter, setter, and adder methods
    Made a method to calculate total price
    
    * Made a method to return an array of all the prices of the items in the cart;
    
    * Made a method to return an array of all the names of the items in the cart;
    
    * Created method to check out all the shopping cart
    
    * Methods to remove products
    
    * Method to return availability of Product
    
    * Method to return availability of Product
    
    * Improved method to return a array of all the availability of Products in the ShoppingCart
    
    * Method to get description
    
    * Changed some methods to private
    refactored itemPrices name
    
    ---------
    
    Co-authored-by: Ahmed Mujtaba <ahmedmujtaba.ult@gmail.com>

---
+++ src/main/java/me/madcabbage/shopping_system/functionalities/ShoppingCart.java
@@ -0,0 +1,147 @@
+package me.madcabbage.shopping_system.functionalities;
+import me.madcabbage.shopping_system.product.Product;
+
+import java.util.ArrayList;
+
+public class ShoppingCart {
+
+    //BuyerUser buyer;
+
+    private double totalPrice;
+    private double discountedPrice;
+    private ArrayList<Product> productArrayList;
+    private double[] itemPrices;
+    private String[] itemNames;
+    private boolean[] productsAvailability;
+    private String[] productsDescription;
+    public ShoppingCart(ArrayList<Product> productArrayList){
+        this.productArrayList = productArrayList;
+        this.totalPrice = calculateTotal();
+        this.itemPrices = calculateAllItemPrices();
+        this.itemNames = itemNames();
+        this.productsAvailability = productsAvailability();
+        this.productsDescription = productsDescription();
+    }
+    public ShoppingCart(Product product){
+        ArrayList<Product> productArrayList = new ArrayList<Product>();
+        productArrayList.add(product);
+        this.productArrayList = productArrayList;
+        this.totalPrice = calculateTotal();
+        this.itemPrices = calculateAllItemPrices();
+        this.itemNames = itemNames();
+        this.productsAvailability = productsAvailability();
+        this.productsDescription = productsDescription();
+    }
+
+    //Getter Methods
+
+    public double getTotalPrice() {
+        return totalPrice;
+    }
+    public ArrayList<Product> getProductArrayList() {
+        return productArrayList;
+    }
+    public double getDiscountedPrice() {
+        return discountedPrice;
+    }
+    public double[] getItemPrices() {
+        return itemPrices;
+    }
+    public String[] getItemNames() {
+        return itemNames;
+    }
+    public boolean[] getProductsAvailability() {
+        return productsAvailability;
+    }
+    public String[] getProductsDescription() {
+        return productsDescription;
+    }
+
+    //Setter Methods
+    public void setProductArrayList(ArrayList<Product> productArrayList) {
+        this.productArrayList = productArrayList;
+    }
+
+    //Adder Methods
+
+    public void addProduct(Product product){
+        this.productArrayList.add(product);
+    }
+    public void addProduct(ArrayList<Product> productArrayList){
+        for (int i = 0; i < productArrayList.size(); i++) {
+            Product product = productArrayList.get(i);
+            addProduct(product);
+        }
+    }
+
+    //product removing methods
+    public void removeProduct(Product productToBeDeleted)
+    {
+        productArrayList.remove(productToBeDeleted);
+    }
+    public void removeProduct(ArrayList<Product> productArrayList){
+
+        for (Product product : productArrayList)
+        {
+            removeProduct(product);
+        }
+    }
+
+    //Method to return total Price
+    private double calculateTotal(){
+        double totalPrice = 0;
+        for (int i = 0; i < productArrayList.size(); i++) {
+            Product product = productArrayList.get(i);
+            totalPrice += product.getPrice();
+        }
+        return totalPrice;
+    }
+
+    //return array of total prices of all the prices of all the products present in the cart
+    private double[] calculateAllItemPrices(){
+        double[] pricesArray = new double[productArrayList.size()];
+        for (int i = 0; i < productArrayList.size(); i++){
+            pricesArray[i] = productArrayList.get(i).getPrice();
+        }
+        return pricesArray;
+    }
+
+    //return array of all the items name
+    private String[] itemNames(){
+        String[] namesArray = new String[productArrayList.size()];
+        for (int i = 0; i < productArrayList.size(); i++){
+            namesArray[i] = productArrayList.get(i).getName();
+        }
+        return namesArray;
+    }
+
+    //Method to check out the shopping cart items
+    public double checkOutItems(double userCash)
+    {
+        if (userCash < totalPrice)
+            return -1;
+        double remaining = userCash - totalPrice;
+        //todo add method to decrease item quantity from the database
+        productArrayList.clear();
+        return remaining;
+    }
+
+    //method to show product availability
+    private boolean[] productsAvailability(){
+        boolean[] availabilityArray = new boolean[productArrayList.size()];
+        for (int i = 0; i < productArrayList.size(); i++){
+            availabilityArray[i] = productArrayList.get(i).isAvailable();
+        }
+        return availabilityArray;
+    }
+
+    //Method to return array of product descriptions
+    private String[] productsDescription(){
+        String[] descriptionsArray = new String[productArrayList.size()];
+        for (int i = 0; i < productArrayList.size(); i++){
+            descriptionsArray[i] = productArrayList.get(i).getDescription();
+        }
+        return descriptionsArray;
+    }
+
+}
