98dd213b50eb2711e9c03d5c2b0b81f9c6ae8200
Ahmed Mujtaba <ahmedmujtaba.ult@gmail.com>
2024-03-16 02:59:33 +0500

    Created class Search.java

---
+++ src/main/java/me/madcabbage/shopping_system/util/Search.java
@@ -0,0 +1,19 @@
+package me.madcabbage.shopping_system.util;
+import me.madcabbage.shopping_system.util.FileDatabase;
+import me.madcabbage.shopping_system.util.Console;
+public class Search {
+
+    /*okay need to search items from the dataBase.
+    * first I got to know how are the items stored
+    * then search for items
+    * this is a static class
+    */
+
+
+
+    private Search()
+    {
+        //hide constructor
+    }
+
+}
